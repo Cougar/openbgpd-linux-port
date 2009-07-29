992cd276c377d91b2c35bf74dee3c0611053620a
Cougar <cougar@random.ee>
2009-07-29 22:08:25 +0300

    comment out IMSG_FILTER_SET and IMSG_IFINFO

--- bgpd/session.c
+++ bgpd/session.c
@@ -2416,6 +2416,7 @@ session_dispatch_imsg(struct imsgbuf *ibuf, int idx, u_int *listener_cnt)
 			pending_reconf = 0;
 			log_info("SE reconfigured");
 			break;
+#if 0
 		case IMSG_IFINFO:
 			if (idx != PFD_PIPE_MAIN)
 				fatalx("IFINFO message not from parent");
@@ -2439,6 +2440,7 @@ session_dispatch_imsg(struct imsgbuf *ibuf, int idx, u_int *listener_cnt)
 					}
 				}
 			break;
+#endif
 		case IMSG_MRT_OPEN:
 		case IMSG_MRT_REOPEN:
 			if (imsg.hdr.len > IMSG_HEADER_SIZE +
