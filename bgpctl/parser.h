831610c12b136c3b4dbbe5a8517a18e79d0364bc
Cougar <cougar@random.ee>
2009-07-29 23:34:08 +0300

    bgpctl updated to OPENBSD_3_8_BASE

--- bgpctl/parser.h
+++ bgpctl/parser.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: parser.h,v 1.7 2004/12/23 17:26:51 henning Exp $ */
+/*	$OpenBSD: parser.h,v 1.9 2005/06/06 17:13:56 henning Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -51,6 +51,8 @@ struct parse_result {
 	enum actions		action;
 	int			flags;
 	u_int8_t		prefixlen;
+	struct filter_set_head	set;
+	sa_family_t		af;
 };
 
 struct parse_result	*parse(int, char *[]);
